67a67a0048ce8f82d373694464fe157131e8b879
Jack Bonatakis <jack@bonatak.is>
2026-03-21 18:18:57 -0400

    Don't leak route info on invalid urls

--- src/frontend/pages/NotFoundPage.svelte
+++ src/frontend/pages/NotFoundPage.svelte
@@ -1,21 +1,13 @@
 <script lang="ts">
   import ErrorState from "../components/ErrorState.svelte";
-  import { onLinkClick, threadsPath } from "../router";
-
-  export let pathname: string;
 </script>
 
 <section class="page">
   <header>
-    <h1 class="page-title" data-route-heading tabindex="-1">Not Found</h1>
-    <p>Route mismatch detected.</p>
+    <h1 class="page-title" data-route-heading tabindex="-1">Page not found</h1>
   </header>
 
-  <ErrorState title="Route not found" message="No route matched this path." detail={pathname} />
-
-  <p class="route-link">
-    <a href={threadsPath} on:click={(event) => onLinkClick(event, threadsPath)}>Go to threads</a>
-  </p>
+  <ErrorState title="There's no page here" message="Check the URL or use the navigation above." />
 </section>
 
 <style>
@@ -25,26 +17,9 @@
     min-width: 0;
   }
 
-  header {
-    display: grid;
-    gap: 0.2rem;
-  }
-
   .page-title {
     margin: 0;
     font-size: 1.2rem;
     color: #102a43;
-  }
-
-  p {
-    margin: 0;
-    color: #486581;
-    line-height: 1.4;
-  }
-
-  .route-link a {
-    color: #0b4ea2;
-    font-weight: 600;
-    text-decoration-thickness: 1px;
   }
 </style>
